727bdfe02f9e1778a1d309add82f744adbebd1f0
Pedro Samuel <128701224+pedrsmc@users.noreply.github.com>
2024-05-09 20:13:30 -0300

    Add files via upload

---
+++ java/poo/modificadores de acesso/agencia bancaria/src/Conta.java
@@ -0,0 +1,37 @@
+package contas;
+
+import java.util.Random;
+
+public class Conta {
+    protected String cliente;
+    protected double saldo;
+
+    protected Conta() {
+        Random agencia = new Random();
+        System.out.println("Agência: " + agencia.nextInt(10000));
+    }
+
+    protected void exibirSaldo() {
+        System.out.printf("Saldo: %.2f\n", this.saldo);
+    }
+    
+    void sacar(double valor) {
+        saldo -= valor;
+    }
+    
+    void depositar(double valor) {
+        saldo += valor;
+    }
+    
+    void transferir(Conta destino, double valor) {
+        this.sacar(valor);
+        destino.depositar(valor);
+    }
+    
+    double soma (double cc1, double cc2) {
+        double total = cc1 + cc2;
+        
+        return total;
+    }
+
+}
